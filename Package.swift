45a31cb09a1092a2a3a536982b4128e4dbaeee88
Kevin Andersen <k.andersen@lego.com>
2018-10-18 10:06:11 -0400

    new package and modulemap files

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "ORSSerialPort",
+    exclude: ["Source/IOKitWrapper"],
     dependencies: [
         .Package(url: "https://github.com/armadsen/SwiftIOKitBridge", majorVersion: 1)
-    ],
-    exclude: ["Source/IOKitWrapper"]
+	]
 )
