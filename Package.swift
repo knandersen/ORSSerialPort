58055642497fa1a31e65fc0ca1e1a8e2e3571f88
Kevin Andersen <k.andersen@lego.com>
2018-10-18 11:50:10 -0400

    added targets to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@ import PackageDescription
 
 let package = Package(
 	name: "ORSSerialPort",
+	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/armadsen/SwiftIOKitBridge", majorVersion: 1)
 	],
