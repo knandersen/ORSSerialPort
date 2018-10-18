999e63b8ad970ad20bd5facb99b448f8338547fb
Kevin Andersen <k.andersen@lego.com>
2018-10-18 13:06:58 -0400

    removed dep

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
 	name: "ORSSerialPort",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/armadsen/SwiftIOKitBridge", majorVersion: 1)
+		
 	],
-	exclude: ["Source/IOKitWrapper"]
 
 )
