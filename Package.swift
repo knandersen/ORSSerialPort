9051064dcd945e95447e0dd13bbf878f4bb224ab
Kevin Andersen <k.andersen@lego.com>
2018-10-18 13:24:21 -0400

    .

--- Package.swift
+++ Package.swift
@@ -2,9 +2,10 @@ import PackageDescription
 
 let package = Package(
 	name: "ORSSerialPort",
-	targets: [],
+	targets:[],
 	dependencies: [
-		
-	]
+		.Package(url: "https://github.com/armadsen/SwiftIOKitBridge", majorVersion: 1)
+	],
+	exclude: ["Source/IOKitWrapper"]
 
 )
