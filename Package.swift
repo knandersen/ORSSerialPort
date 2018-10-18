d7e82dc3f489db0ff962fece059a73a3a464f342
Kevin Andersen <k.andersen@lego.com>
2018-10-18 15:49:13 -0400

    removed targets from Package

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@ import PackageDescription
 
 let package = Package(
 	name: "ORSSerialPort",
-	targets:[],
 	dependencies: [
 		.Package(url: "https://github.com/armadsen/SwiftIOKitBridge", majorVersion: 1)
 	],
