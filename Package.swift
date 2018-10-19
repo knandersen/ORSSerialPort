1e9fd9c8bfe26d20ddc95d4ea25a61e276a88fa7
Kevin Andersen <k.andersen@lego.com>
2018-10-19 12:55:19 -0400

    restructured

--- Package.swift
+++ Package.swift
@@ -13,13 +13,5 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/armadsen/SwiftIOKitBridge.git", .exact("1.0.0"))
-	],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ORSSerialPort",
-            dependencies: ["SwiftIOKitBridge"],
-            exclude: ["Source/IOKitWrapper"])
-    ]
+	]
 )
